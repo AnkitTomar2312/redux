fa2a91f079e03042d6441964af432a34e3609962
Ankit Tomar <ankitjdec@gmail.com>
2023-02-20 21:30:02 +0530

    basic increase and decrease app with out redux

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <span>Your Current balance is</span>
+      <h1>10000</h1>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <button>-</button>
+      <sapn>Your Total is</sapn>
+      <button>+</button>
+    </div>
+  );
+};
+
+export default Shop;
